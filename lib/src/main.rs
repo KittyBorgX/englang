4aa5d81e42ba41f6386c341f1b85832aaaa599e6
Krishna Ramasimha <krishnaramasimha12@gmail.com>
2021-06-21 20:40:00 +0530

    Update main.rs

--- lib/src/main.rs
+++ lib/src/main.rs
@@ -1,3 +1,15 @@
+//===----------------- main.rs - Main file, initial file --------------------===//
+//
+// This source file is part of the englang open source project
+//
+// Copyright (c) 2021 KittyX29 and the englang project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://github.com/KittyX29/englang/blob/main/LICENSE for license information
+// See https://github.com/KittyX29/englang/blob/main/CONTRIBUTORS.md for the list of englang project authors
+//
+//===----------------------------------------------------------------------===//
+
 use logos::Logos;
 use std::fs::File;
 use std::io::prelude::*;
